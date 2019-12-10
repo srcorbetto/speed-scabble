f39c29552aa71471e6220665fd7a21fc24d3141d
Mike Corbett <srcorbetto@gmail.com>
2019-12-09 21:28:57 -0600

    Added drawing letters

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,156 +2,473 @@ let game = {
     letters: [
         {
             letter: '?',
-            quantity: 2,
+            points: 0
+        },
+        {
+            letter: '?',
             points: 0
         },
         {
             letter: 'e',
-            quantity: 12,
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
+            points: 1
+        },
+        {
+            letter: 'e',
             points: 1
         },
         {
             letter: 'a',
-            quantity: 9,
+            points: 1
+        },
+        {
+            letter: 'a',
+            points: 1
+        },
+        {
+            letter: 'a',
+            points: 1
+        },
+        {
+            letter: 'a',
+            points: 1
+        },
+        {
+            letter: 'a',
+            points: 1
+        },
+        {
+            letter: 'a',
+            points: 1
+        },
+        {
+            letter: 'a',
+            points: 1
+        },
+        {
+            letter: 'a',
+            points: 1
+        },
+        {
+            letter: 'a',
+            points: 1
+        },
+        {
+            letter: 'i',
+            points: 1
+        },
+        {
+            letter: 'i',
+            points: 1
+        },
+        {
+            letter: 'i',
+            points: 1
+        },
+        {
+            letter: 'i',
+            points: 1
+        },
+        {
+            letter: 'i',
+            points: 1
+        },
+        {
+            letter: 'i',
+            points: 1
+        },
+        {
+            letter: 'i',
+            points: 1
+        },
+        {
+            letter: 'i',
+            points: 1
+        },
+        {
+            letter: 'i',
             points: 1
         },
         {
             letter: 'o',
-            quantity: 8,
+            points: 1
+        },
+        {
+            letter: 'o',
+            points: 1
+        },
+        {
+            letter: 'o',
+            points: 1
+        },
+        {
+            letter: 'o',
+            points: 1
+        },
+        {
+            letter: 'o',
+            points: 1
+        },
+        {
+            letter: 'o',
+            points: 1
+        },
+        {
+            letter: 'o',
+            points: 1
+        },
+        {
+            letter: 'o',
             points: 1
         },
         {
             letter: 'n',
-            quantity: 6,
+            points: 1
+        },
+        {
+            letter: 'n',
+            points: 1
+        },
+        {
+            letter: 'n',
+            points: 1
+        },
+        {
+            letter: 'n',
+            points: 1
+        },
+        {
+            letter: 'n',
+            points: 1
+        },
+        {
+            letter: 'n',
             points: 1
         },
         {
             letter: 'r',
-            quantity: 6,
+            points: 1
+        },
+        {
+            letter: 'r',
+            points: 1
+        },
+        {
+            letter: 'r',
+            points: 1
+        },
+        {
+            letter: 'r',
+            points: 1
+        },
+        {
+            letter: 'r',
+            points: 1
+        },
+        {
+            letter: 'r',
             points: 1
         },
         {
             letter: 't',
-            quantity: 6,
+            points: 1
+        },
+        {
+            letter: 't',
+            points: 1
+        },
+        {
+            letter: 't',
+            points: 1
+        },
+        {
+            letter: 't',
+            points: 1
+        },
+        {
+            letter: 't',
+            points: 1
+        },
+        {
+            letter: 't',
             points: 1
         },
         {
             letter: 'l',
-            quantity: 4,
+            points: 1
+        },
+        {
+            letter: 'l',
+            points: 1
+        },
+        {
+            letter: 'l',
+            points: 1
+        },
+        {
+            letter: 'l',
             points: 1
         },
         {
             letter: 's',
-            quantity: 4,
+            points: 1
+        },
+        {
+            letter: 's',
+            points: 1
+        },
+        {
+            letter: 's',
+            points: 1
+        },
+        {
+            letter: 's',
             points: 1
         },
         {
             letter: 'u',
-            quantity: 4,
+            points: 1
+        },
+        {
+            letter: 'u',
+            points: 1
+        },
+        {
+            letter: 'u',
+            points: 1
+        },
+        {
+            letter: 'u',
             points: 1
         },
         {
             letter: 'd',
-            quantity: 4,
+            points: 2
+        },
+        {
+            letter: 'd',
+            points: 2
+        },
+        {
+            letter: 'd',
+            points: 2
+        },
+        {
+            letter: 'd',
             points: 2
         },
         {
             letter: 'g',
-            quantity: 3,
+            points: 2
+        },
+        {
+            letter: 'g',
+            points: 2
+        },
+        {
+            letter: 'g',
             points: 2
         },
         {
             letter: 'b',
-            quantity: 2,
+            points: 3
+        },
+        {
+            letter: 'b',
             points: 3
         },
         {
             letter: 'c',
-            quantity: 2,
+            points: 3
+        },
+        {
+            letter: 'c',
             points: 3
         },
         {
             letter: 'm',
-            quantity: 2,
+            points: 3
+        },
+        {
+            letter: 'm',
             points: 3
         },
         {
             letter: 'p',
-            quantity: 2,
+            points: 3
+        },
+        {
+            letter: 'p',
             points: 3
         },
         {
             letter: 'f',
-            quantity: 2,
+            points: 4
+        },
+        {
+            letter: 'f',
             points: 4
         },
         {
             letter: 'h',
-            quantity: 2,
+            points: 4
+        },
+        {
+            letter: 'h',
             points: 4
         },
         {
             letter: 'v',
-            quantity: 2,
+            points: 4
+        },
+        {
+            letter: 'v',
             points: 4
         },
         {
             letter: 'w',
-            quantity: 2,
+            points: 4
+        },
+        {
+            letter: 'w',
             points: 4
         },
         {
             letter: 'y',
-            quantity: 2,
+            points: 4
+        },
+        {
+            letter: 'y',
             points: 4
         },
         {
             letter: 'k',
-            quantity: 1,
             points: 5
         },
         {
             letter: 'j',
-            quantity: 1,
             points: 8
         },
         {
             letter: 'x',
-            quantity: 1,
             points: 8
         },
         {
             letter: 'q',
-            quantity: 1,
             points: 10
         },
         {
             letter: 'z',
-            quantity: 1,
             points: 10
         }
 
-    ],
-    playersLetters: null,
-    giveStartingLetters: null,
-
+    ]
 };
 
-console.log(game);
+let playerLetters = [];
+
+const getPlayerLetters = () => {
+    for (i = 0; i < 50; i++) {
+        console.log(`length: ${game.letters.length}`);
+        const randNum = Math.floor(Math.random() * (game.letters.length));
+        console.log(`length: ${game.letters.length}`);
+        console.log(`number: ${randNum}`);
+        const letter = {
+            letter: game.letters[randNum].letter,
+            points: game.letters[randNum].points
+        };
+        console.log(`length: ${game.letters.length}`);
+        playerLetters.push(letter);
+        console.log(`length: ${game.letters.length}`);
+        game.letters.splice(randNum, 1);
+        console.log(`length: ${game.letters.length}`);
+        console.log(`=====================================================`);
+        
+        // console.log(`length: ${game.letters.length}`);
+        // const randNum = Math.floor(Math.random() * (game.letters.length + 1));
+        // console.log(`length: ${game.letters.length}`);
+        // game.letters.splice(randNum, 1);
+        // console.log(`length: ${game.letters.length}`);
+        // console.log(`=====================================================`);
+
+    }
+    // console.log(playerLetters)
+    // console.log(game.letters)
+}
+
+const drawLetter = () => {
+}
+
+const pickLetter = num => {
+    if (game.letters[randNum].quantity > 0) {
+        const letter = {
+            letter: game.letters[randNum].letter,
+            points: game.letters[randNum].points
+        };
+        playerLetters.push(letter);
+        game.letters[randNum].quantity--;
+    } else {
+        
+    }
+}
 
 $('.goal').droppable({
-    accept: '.box',
+    accept: '.letter',
     drop: () => {
         console.log('POOP')
     }
 });
 
 game.letters.forEach(letterTile => {
-    for (i = 0; i < letterTile.quantity; i++) {
-        $('body').append(`<div class="letter"><p>${letterTile.letter}<p></div>`)
-    }
+    $('body').append(`<div class="letter"><p>${letterTile.letter}<p></div>`)
 })
 
 $('.letter').draggable({
-    snap: true
+    snap: true,
+    stop: (event, ui) => {
+        console.log(event.target)
+        console.log(ui)
+    }
 });
+
+getPlayerLetters();
+
