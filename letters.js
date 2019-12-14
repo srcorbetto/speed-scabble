7338c6874c5cc083c9558d8ec22fc7b19f66ea1b
Mike Corbett <srcorbetto@gmail.com>
2019-12-14 15:41:36 -0600

    Players can draw from same letters

--- letters.js
+++ letters.js
@@ -1,9 +1,4 @@
-const socket = io();
-socket.on('draw', msg => {
-    console.log(msg);
-})
-
-let letters = [
+const letters = [
     {
         letter: '?',
         points: 0
@@ -407,74 +402,4 @@ let letters = [
 
 ];
 
-let playerLetters = [];
-
-const getPlayerLetters = numberOfLettersNeeded => {
-    $.get( "/letters", lettersFromServer => {
-        for (i = 0; i < numberOfLettersNeeded; i++) {
-            const randNum = Math.floor(Math.random() * (lettersFromServer.length));
-            playerLetters.push(lettersFromServer[randNum]);
-            lettersFromServer.splice(randNum, 1);
-        }
-        const updatedLetters = {
-            letters: lettersFromServer
-        };
-        $.post('/letters-update', updatedLetters, data => {
-            console.log(data);
-            playerLetters.forEach(letterTile => {
-                $('body').append(`<div class="letter">${letterTile.letter}</div>`)
-            });
-            makeDraggable();
-        });
-    });
-}
-
-const drawLetter = () => {
-    $.get( "/letters", lettersFromServer => {
-        for (i = 0; i < 1; i++) {
-            const randNum = Math.floor(Math.random() * (lettersFromServer.length));
-            playerLetters.push(lettersFromServer[randNum]);
-            lettersFromServer.splice(randNum, 1);
-        }
-        const updatedLetters = {
-            letters: lettersFromServer
-        };
-        $.post('/letters-update', updatedLetters, data => {
-            console.log(data);
-            const lastLetterInArray = playerLetters[playerLetters.length - 1].letter;
-            $('body').append(`<div class="letter">${lastLetterInArray}</div>`);
-            makeDraggable();
-            socket.emit('draw', 'draw');
-        });
-    });
-}
-
-const makeDraggable = () => {
-    $('.letter').draggable({
-        snap: '.grid',
-        stop: (event, ui) => {
-        }
-    });
-}
-
-const initGrid = () => {
-    for (i = 0; i < 14; i++) {
-        $('.hr-container').append(`<hr class="line grid">`);
-        $('.vr-container').append(`<div class="line-vert grid"></div>`);
-    }
-}
-
-$('.container-wrapper').droppable({
-    accept: '.letter',
-    drop: (e, ui) => {
-        console.log(e.target);
-        console.log(ui.draggable[0]);
-    }
-});
-
-initGrid();
-getPlayerLetters(7);
-$('.draw').on('click', () => {
-    drawLetter();
-})
-
+module.exports.letters = letters;
--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 const port = process.env.PORT || 5000;
+let letters = require('./letters').letters;
+
+console.log(letters);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/' + '/index.html'));
 });
 
+app.get('/letters', (req, res) => {
+    res.send(letters);
+});
+
+app.post('/letters-update', (req, res) => {
+    letters = req.body.letters;
+    res.send(letters);
+})
+
 io.on('connection', socket => {
     console.log('a user connected');
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        letters = require('./letters').letters;
     });
     socket.on('draw', msg => {
         console.log(msg);
